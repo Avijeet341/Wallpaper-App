788beba42c137a9cab1083206142566fab41c37d
Avijeet#21 <80438115+Avijeet341@user.noreply.github.com>
2023-08-31 14:14:41 +0530

    first commit

---
+++ app/src/main/java/com/avi/imagetest/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.avi.imagetest
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val constraintLayout: ConstraintLayout = findViewById(R.id.imageLayout)
+        val animationDrawable: AnimationDrawable = constraintLayout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2500)
+        animationDrawable.setExitFadeDuration(3000)
+        animationDrawable.start()
+    }
+}
